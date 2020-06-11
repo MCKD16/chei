a201e2fea10ca62eba124f0d8b3d6ed71e37ad45
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-06-11 22:34:23 +0900

    Update py.py

--- py.py
+++ py.py
@@ -6,7 +6,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.content.startswith('#help'):
+  if message.startswith('#help'):
     await client.send_message(message.channel, '명령어 목록:\n```#help - 명령어 목록을 확인합니다.```')
 
 access_token = os.environ["BOT_TOKEN"]
